42f1a7d360ba10940fdd19751a954ec86586cb7d
itachi-flamenguista <105893881+itachi-flamenguista@users.noreply.github.com>
2022-10-25 19:34:52 -0300

    TempoEspera1.0

--- Time_3/Assets/Scripts/Player/PlayerCollect.cs
+++ Time_3/Assets/Scripts/Player/PlayerCollect.cs
@@ -9,19 +9,37 @@ public class PlayerCollect : MonoBehaviour
 
     public GameObject exitIndicator;
 
+    public bool podeColetar;
+
+    public float tempoEspera = 5f;
+
     private void Start()
     {
         playerStatus = GetComponent<PlayerStatus>();
+        podeColetar = false;
+        
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Dvd")
+        if (collision.tag == "Dvd" )
         {
-            playerStatus.hasDVD = true;
-            if(SceneManager.GetActiveScene().name == "Teste0")
-                exitIndicator.SetActive(true);
-            Destroy(collision.gameObject);
+            StartCoroutine(Esperar(tempoEspera)); 
+            if(podeColetar == true)
+            {
+                playerStatus.hasDVD = true;
+                if(SceneManager.GetActiveScene().name == "Teste0")
+                    exitIndicator.SetActive(true);
+                Destroy(collision.gameObject);
+                podeColetar = false;
+            }
+           
         }
+    }
+
+    IEnumerator Esperar(float tempoEspera)
+    {
+        yield return new WaitForSeconds (tempoEspera);
+        podeColetar = true;
     }
 
 }
