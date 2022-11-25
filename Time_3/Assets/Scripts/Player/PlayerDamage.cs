5db5453f768b79849faeed90518303d7f7bec203
Mcesteves <71904551+Mcesteves@users.noreply.github.com>
2022-11-25 20:08:02 -0300

    Implementa barra de detecção do player

--- Time_3/Assets/Scripts/Player/PlayerDamage.cs
+++ Time_3/Assets/Scripts/Player/PlayerDamage.cs
@@ -2,47 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerDamage : MonoBehaviour
 {
-    [Tooltip("Tempo que o player pode permanecer na area de perigo sem ser afetado.")]
-    [SerializeField]
-    private float timeLimit;
-    private bool inDanger;
+    public Slider playerDetectionBar;
+    public float detectionRate;
+    public float maxDetectionValue;
     private PlayerSlingshotMovement playerSlingshot;
     private PlayerStatus playerStatus;
+    private bool inDanger;
 
     void Start()
     {
         playerSlingshot = GetComponent<PlayerSlingshotMovement>();
         playerStatus = GetComponent<PlayerStatus>();
+        playerDetectionBar.maxValue = maxDetectionValue;
+        playerDetectionBar.value = 0f;
+    }
+
+    private void Update()
+    {
+        if (inDanger)
+            IncreaseDetectionBar();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Danger")
-            StartCoroutine(StartTimer());
+        if(collision.tag == "Danger")
+        {
+            inDanger = true;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.tag == "Danger")
-        {
-            inDanger = false;
-            StopCoroutine(StartTimer());
-        }
+        inDanger = false;
     }
 
     private IEnumerator StartTimer()
     {
-        inDanger = true;
-        yield return new WaitForSeconds(timeLimit);
-        if (inDanger)
-        {
             playerSlingshot.onSlingshot = false;
             playerStatus.fade.GetComponent<Animator>().Play("FadeIn");
             yield return new WaitForSeconds(1.0f);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }   
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);  
+    }
+
+    private void IncreaseDetectionBar()
+    {
+        if( playerDetectionBar.value == maxDetectionValue)
+        {
+            StartCoroutine(StartTimer());
+            return;
+        }
+
+        playerDetectionBar.value += detectionRate*Time.deltaTime;
     }
 }
